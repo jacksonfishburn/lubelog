dc700b7b418a733c1f79b7d452559a09edfa460e
jacksonfishburn <jacksonfishburn@gmail.com>
2026-07-13 16:51:15 -0600

    test: GeminiClient

--- backend/src/main/java/dev/jacksonfishburn/lubelog/client/model/ai/GeminiRequest.java
+++ backend/src/main/java/dev/jacksonfishburn/lubelog/client/model/ai/GeminiRequest.java
@@ -1,9 +1,11 @@
 package dev.jacksonfishburn.lubelog.client.model.ai;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public record GeminiRequest(
         String model,
         String input,
